19dd4cb2a1441fd821d9bd6f546e5f648b4759d3
Jorge Fernández <164742315+jofmar00@users.noreply.github.com>
2024-04-23 13:08:12 -0400

    Added title and code readability

--- portScan.sh
+++ portScan.sh
@@ -1,38 +1,55 @@
 #!/bin/bash
 
 #Colours
-greenColour="\e[0;32m\033[1m"
+green="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
+red="\e[0;31m\033[1m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
+purple="\e[0;35m\033[1m"
+turquoise="\e[0;36m\033[1m"
+gray="\e[0;37m\033[1m"
 deleteLine="\033[K"
+
+#Title
+function printTitle(){
+	echo "                      __  _____                "
+	echo "    ____  ____  _____/ /_/ ___/_________ _____ "
+	echo "   / __ \/ __ \/ ___/ __/\__ \/ ___/ __ \`/ __ \\"
+	echo "  / /_/ / /_/ / /  / /_ ___/ / /__/ /_/ / / / /"
+	echo " / .___/\____/_/   \__//____/\___/\__,_/_/ /_/ "
+	echo "/_/    "
+	echo -e "\n By Jofmar00"
+}
+
+#Scan function
+function scan(){
+	bash -c "echo '' > /dev/tcp/$ip/$port" 2> /dev/null && 
+	echo -e "${deleteLine}${green}[+]${endColour} ${gray}Puerto ${endColour}${blue}$port${endColour}${gray} - OPEN${endColour}" &
+}
+
 
 #Signal Managment
 function ctrl_c(){
-	echo -e "$\n\n${redColour}[!]${endColour} ${grayColour}Exiting scan...${endColour}\n"
+	echo -e "$\n\n${red}[!]${endColour} ${gray}Exiting scan...${endColour}\n"
 	exit 1
 }
-
+############   --- MAIN PROGRAM --- #########
 #Argument check
 if [ $# -ne 1 ]; then
 	echo "Usage: ./portScan.sh <ipAddr>"
-	exit 1 
-fi 
+	exit 1
+fi
+
 #Ctrl-C
 trap ctrl_c SIGINT
-
-#Main program
+printTitle
 ip=$1
-echo -e "${yellowColour}Starting port scan in $ip...${endColour}" 
+echo -e "${yellow}Starting port scan in $ip...${endColour}"
 for port in $(seq 1 65535);
-do 
-	timeout 1 bash -c "echo '' > /dev/tcp/$ip/$port" 2> /dev/null && echo -e "${deleteLine}${greenColour}[+]${endColour} ${grayColour}Puerto ${endColour}${blueColour}$port${endColour}${grayColour} - OPEN${endColour}" &
-
-	echo -ne "\rScanning port: ${blueColour} $port / 65535 ${endColour} \r"
+do
+	scan
+	echo -ne "\rScanning port: ${blue} $port / 65535 ${endColour} \r"
 done
 
-echo "${greenColour}FINISHED SCAN${endColour}"
+echo "${green}FINISHED SCAN${endColour}"
